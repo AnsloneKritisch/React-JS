3f19477e6b5ab40c76d5535e63c5f1bedb29cc73
AnsloneKritisch <digveshoptimus@gmail.com>
2023-01-25 21:36:02 +0530

    new

--- 03_Properties_of_React/pro/src/index.js
+++ 03_Properties_of_React/pro/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react"
+import ReactDom from "react-dom"
+import Outside from "./sample_comp"
+import SampleCode from "./test_compo"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Header()
+{
+  return <div style={{ textAlign:'center'}}><h1 style={{color:"blue"}}> This is my Heading </h1></div>
+}
+
+ReactDom.render( <div> <Header></Header> <Outside></Outside>   <SampleCode name="Anslone" age="18"></SampleCode>  <SampleCode name="Anshu" age="15"></SampleCode><SampleCode name="Palku" age="29"></SampleCode><SampleCode name="Palansh" age="27"></SampleCode><SampleCode name="Kritisch" age="65" ></SampleCode> <Header></Header> </div> ,  document.getElementById("root"))
---
+++ 03_Properties_of_React/pro/src/test_compo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SampleCode(props)
+{
+    return <h1 style={{textAlign:'center'}}> Hello Its <span style={{color:'red'}}>{props.name}</span> and my age is  <span style={{color:'red'}}>{props.age}</span>   </h1>
+}
+
+export default SampleCode
