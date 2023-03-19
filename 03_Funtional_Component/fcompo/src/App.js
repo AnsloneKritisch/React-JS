883ac433ee65a44e4d1f51338b9c922f4bd50604
AnsloneKritisch <digveshoptimus@gmail.com>
2023-03-19 09:12:37 +0530

    compo

---
+++ 03_Funtional_Component/fcompo/src/App.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+import './App.css';
+import Greet from './compo/greet';
+import { Es6 } from './compo/greet';
+
+function App() {
+  return (
+    <div className="App">
+    <Greet></Greet>
+    <Es6></Es6>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ 03_Funtional_Component/fcompo/src/compo/greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Greet()
+{
+    return <h1>Hello Anslone</h1>
+}
+
+export const Es6 = ( ) => <h1> Yup ! it Es6 Component </h1>
+// This is Name Expoerts 
+
+export default Greet
+
+
